2bc1212db6637eef3a6367bf8e75c4bf93498780
CUCS <2318002579@qq.com>
2024-01-30 16:31:36 -0800

    finish extract_next_links and is_valid function

--- crawler.py
+++ crawler.py
@@ -1,6 +1,7 @@
 import logging
 import re
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urljoin
+from lxml import html, etree
 
 logger = logging.getLogger(__name__)
 
@@ -29,7 +30,12 @@ class Crawler:
                     if self.corpus.get_file_name(next_link) is not None:
                         self.frontier.add_url(next_link)
 
-    def extract_next_links(self, url_data):
+    @staticmethod
+    def is_absolute_url(url):
+        parsed_url = urlparse(url)
+        return bool(parsed_url.scheme and parsed_url.netloc)
+
+    def extract_next_links(self, url_data: dict):
         """
         The url_data coming from the fetch_url method will be given as a parameter to this method. url_data contains the
         fetched url, the url content in binary format, and the size of the content in bytes. This method should return a
@@ -39,8 +45,24 @@ class Crawler:
 
         Suggested library: lxml
         """
-        outputLinks = []
-        return outputLinks
+        if url_data['content'] is None or not url_data['content'].strip() or url_data['http_code'] != 200:
+            return []
+
+        content = url_data['content']
+        base_url = url_data['final_url'] if url_data['is_redirected'] else url_data['url']
+
+        try:
+            tree = html.fromstring(content)
+        except etree.ParserError:
+            return []
+
+        links = []
+        for element in tree.xpath('//a/@href'):
+            link = element.strip()
+            if link:
+                absolute_url = urljoin(base_url, link)
+                links.append(absolute_url)
+        return links
 
     def is_valid(self, url):
         """
@@ -52,14 +74,27 @@ class Crawler:
         if parsed.scheme not in set(["http", "https"]):
             return False
         try:
-            return ".ics.uci.edu" in parsed.hostname \
-                   and not re.match(".*\.(css|js|bmp|gif|jpe?g|ico" + "|png|tiff?|mid|mp2|mp3|mp4" \
-                                    + "|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf" \
-                                    + "|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso|epub|dll|cnf|tgz|sha1" \
-                                    + "|thmx|mso|arff|rtf|jar|csv" \
-                                    + "|rm|smil|wmv|swf|wma|zip|rar|gz|pdf)$", parsed.path.lower())
+            if ".ics.uci.edu" not in parsed.hostname:
+                return False
 
+            if re.search(r"\.(css|js|bmp|gif|jpeg|jpg|ico|png|tiff|mid|mp2|mp3|mp4"
+                         r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
+                         r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso|epub|dll|cnf|tgz|sha1"
+                         r"|thmx|mso|arff|rtf|jar|csv"
+                         r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower()):
+                return False
         except TypeError:
             print("TypeError for ", parsed)
             return False
 
+        if len(url) > 200:
+            return False
+
+        path_segments = parsed.path.split('/')
+        if any(path_segments.count(segment) > 1 for segment in path_segments):
+            return False
+
+        if len(parsed.query.split('&')) > 10:
+            return False
+
+        return True
