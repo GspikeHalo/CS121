3faddc0414311cadfef3f949b051dd875f25308b
Shenghao Fu <2318002579@qq.com>
2024-03-04 11:21:22 -0800

    sth

--- search_engine/engine/connection_pool.py
+++ search_engine/engine/connection_pool.py
@@ -3,11 +3,11 @@ from queue import Queue
 
 
 class SQLiteConnectionPool:
-    def __init__(self, database_path, pool_size=5):
+    def __init__(self, database_path, pool_size=5, timeout=20):
         self.pool = Queue(maxsize=pool_size)
         self.database_path = database_path
         for _ in range(pool_size):
-            conn = sqlite3.connect(database_path, check_same_thread=False)
+            conn = sqlite3.connect(database_path, check_same_thread=False, timeout=timeout)
             self.pool.put(conn)
 
     def get_connection(self):
