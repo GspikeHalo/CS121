c33aaa7e45aa0765025792ac7ac49d7380215b2e
Shenghao Fu <2318002579@qq.com>
2024-03-08 18:51:46 -0800

    sth

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from search_engine.events.event_bus import EventBus
 from search_engine.engine.main import Engine
 from search_engine.views.main import MainView
 
+
 def main():
     event_bus = EventBus()
     engine = Engine()
@@ -10,6 +11,7 @@ def main():
     event_bus.set_view(main_view)
     main_view.set_db("./")
     main_view.run()
+
 
 if __name__ == '__main__':
     main()
